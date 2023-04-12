df950e01312f0f118078009ac0a45a8cee8ffbf3
siddarthabiswas <sidubiswas6@gmail.com>
2023-04-12 17:07:31 +0600

    added blog area

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -13,7 +13,7 @@ const Blog = () => {
                 <p className='mb-8'>Custom React JS hooks are reusable functions that a React JS software developer can use to add special and unique functionality to the React applications. Usually, if there is a requirement to add a feature, one can install a third-party library and solve the problem.</p>
                 <h4  className='text-5xl mb-5'>What is useref ?</h4>
                 <p className='mb-8'>❮ Previous Next ❯ The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</p>
-                <h4  className='text-5xl mb-5'>what is usememo ?</h4>
+                <h4  className='text-5xl mb-5'>What is usememo ?</h4>
                 <p className='mb-8'>useMemo is a React Hook that lets you cache the result of a calculation between re-renders. const cachedValue = useMemo(calculateValue, dependencies) Reference. useMemo(calculateValue, dependencies)</p>
             </div>
         </div>
